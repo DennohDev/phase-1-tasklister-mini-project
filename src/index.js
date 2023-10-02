553af63a77fca540f48f65044594590c26c426d7
Dennis Maina <denniskamwene1@gmail.com>
2023-10-02 09:42:43 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ form.addEventListener("submit", function(event) {
   const listItem = document.createElement("li");
   listItem.textContent = taskDescription; 
 
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
+  deleteButton.addEventListener("click", function() {
+    listItem.remove(); 
+  });
+
   tasksList.appendChild(listItem);
+
+  listItem.appendChild(deleteButton);
+
 
   document.getElementById("new-task-description").value = "";
 
